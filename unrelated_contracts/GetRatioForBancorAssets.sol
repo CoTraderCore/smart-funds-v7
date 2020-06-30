b2e8619ea8f8dc13d528bd6b7cf0d21b9e79b4a2
Ruslan <ruslan777mirov@gmail.com>
2020-06-30 13:57:42 +0300

    update unrelated_contracts

--- unrelated_contracts/GetRatioForBancorAssets.sol
+++ unrelated_contracts/GetRatioForBancorAssets.sol
@@ -1,9 +1,45 @@
 pragma solidity ^0.4.24;
 
-import "./interfaces/PathFinderInterface.sol";
-import "./interfaces/BancorNetworkInterface.sol";
-import "./interfaces/IGetBancorAddressFromRegistry.sol";
-import "../zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../contracts/bancor/interfaces/PathFinderInterface.sol";
+import "../contracts/bancor/interfaces/BancorNetworkInterface.sol";
+import "../contracts/bancor/interfaces/IGetBancorAddressFromRegistry.sol";
+
+pragma solidity ^0.4.24;
+
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * See https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender)
+    public view returns (uint256);
+
+  function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
 
 
 contract GetRatioForBancorAssets {
