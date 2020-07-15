8739214f13500b277fac6a5c06303bb97126fff6
Ruslan <ruslan777mirov@gmail.com>
2020-07-15 11:27:10 +0300

    Update unrelated contracts

--- unrelated_contracts/GetBancorAddressFromRegistry.sol
+++ unrelated_contracts/GetBancorAddressFromRegistry.sol
@@ -1,7 +1,26 @@
 pragma solidity ^0.4.24;
 
-import "../contracts/bancor/interfaces/IContractRegistry.sol";
-import "./helpers/stringToBytes32.sol";
+library stringToBytes32 {
+  function convert(string memory source) internal pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        result := mload(add(source, 32))
+    }
+   }
+}
+interface IContractRegistry {
+    function addressOf(bytes32 _contractName) external view returns (address);
+    // deprecated, backward compatibility
+    function getAddress(bytes32 _contractName) external view returns (address);
+}
+
+
+
+
 
 /**
  * @title Ownable
--- unrelated_contracts/GetRatioForBancorAssets.sol
+++ unrelated_contracts/GetRatioForBancorAssets.sol
@@ -1,45 +1,127 @@
 pragma solidity ^0.4.24;
 
-import "../contracts/bancor/interfaces/PathFinderInterface.sol";
-import "../contracts/bancor/interfaces/BancorNetworkInterface.sol";
-import "../contracts/bancor/interfaces/IGetBancorAddressFromRegistry.sol";
-
-pragma solidity ^0.4.24;
-
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * See https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
+interface IGetBancorAddressFromRegistry {
+  function getBancorContractAddresByName(string _name) external view returns (address result);
 }
 
 
-
-
 /**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
+ * @dev Interface of the ERC20 standard as defined in the EIP.
  */
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender)
-    public view returns (uint256);
+interface ERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
 
-  function transferFrom(address from, address to, uint256 value)
-    public returns (bool);
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
 
-  function approve(address spender, uint256 value) public returns (bool);
-  event Approval(
-    address indexed owner,
-    address indexed spender,
-    uint256 value
-  );
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
+/*
+    Bancor Network interface
+*/
+interface BancorNetworkInterface {
+   function getReturnByPath(
+     ERC20[] _path,
+     uint256 _amount)
+     external
+     view
+     returns (uint256, uint256);
+
+    function convert(
+        ERC20[] _path,
+        uint256 _amount,
+        uint256 _minReturn
+    ) external payable returns (uint256);
+
+    function claimAndConvert(
+        ERC20[]  _path,
+        uint256 _amount,
+        uint256 _minReturn
+    ) external returns (uint256);
+
+    function convertFor(
+        ERC20[]  _path,
+        uint256 _amount,
+        uint256 _minReturn,
+        address _for
+    ) external payable returns (uint256);
+
+    function claimAndConvertFor(
+        ERC20[]  _path,
+        uint256 _amount,
+        uint256 _minReturn,
+        address _for
+    ) external returns (uint256);
+
+}
+
+interface PathFinderInterface {
+ function generatePath(address _sourceToken, address _targetToken) external view returns (address[] memory);
+}
+
 
 
 contract GetRatioForBancorAssets {
