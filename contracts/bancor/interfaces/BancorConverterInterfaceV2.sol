729316e65519e8e9dab26874b9faa057e25dfffd
Ruslan <ruslan777mirov@gmail.com>
2020-08-13 12:31:15 +0300

    Update Bancor interface

--- contracts/bancor/interfaces/BancorConverterInterfaceV2.sol
+++ contracts/bancor/interfaces/BancorConverterInterfaceV2.sol
@@ -1,8 +1,8 @@
 import "../../zeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 interface BancorConverterInterfaceV2 {
-  function addLiquidity(address _reserveToken, uint256 _amount) external payable;
-  function removeLiquidity(address _poolToken, uint256 _amount) external;
+  function addLiquidity(IERC20 _reserveToken, uint256 _amount, uint256 _minReturn) external payable;
+  function removeLiquidity(address _poolToken, uint256 _amount, uint256 _minReturn) external;
 
   function connectorTokenCount() external view returns (uint16);
   function connectorTokens(uint index) external view returns(IERC20);
