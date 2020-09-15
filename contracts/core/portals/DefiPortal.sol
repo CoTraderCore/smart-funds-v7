17454105b207bed74bd04fe2680e91730dc1ea92
Ruslan <ruslan777mirov@gmail.com>
2020-09-15 14:21:44 +0300

    Implement Compound in DEFI portal

--- contracts/core/portals/DefiPortal.sol
+++ contracts/core/portals/DefiPortal.sol
@@ -2,20 +2,30 @@
 pragma solidity ^0.6.12;
 
 import "../../zeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../../zeppelin-solidity/contracts/math/SafeMath.sol";
+
+import "../interfaces/ITokensTypeStorage.sol";
 import "../../compound/CToken.sol";
 import "../../compound/CEther.sol";
 
 
 contract DefiPortal {
+  using SafeMath for uint256;
+
   // COMPOUND ETH wrapper address
   CEther public cEther;
+  address constant private ETH_TOKEN_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+
+  // Contract for handle tokens types
+  ITokensTypeStorage public tokensTypes;
 
   // Enum
   // NOTE: You can add a new type at the end, but DO NOT CHANGE this order
   enum DefiActions { CompoundLoan, CompoundReedem }
 
-  constructor(address _cEther) public {
+  constructor(address _cEther, address _tokensTypes) public {
     cEther = CEther(_cEther);
+    tokensTypes = ITokensTypeStorage(_tokensTypes);
   }
 
   function callPayableProtocol(
@@ -32,7 +42,17 @@ contract DefiPortal {
       uint256[] memory amountReceived
     )
   {
+    if(uint(_additionalArgs[0]) == uint(DefiActions.CompoundLoan)){
+      (tokensSent,
+       tokensReceived,
+       amountSent,
+       amountReceived) = compoundMint(_data);
 
+       eventType = "COMPOUND_LOAN";
+    }
+    else{
+      revert("Unknown action");
+    }
   }
 
   function callNonPayableProtocol(
@@ -41,14 +61,32 @@ contract DefiPortal {
   )
     external
     returns(
-      string memory eventType
-      address[] memory tokensSent
-      address[] memory tokensReceived
-      uint256[] memory amountSent
-      uint256[] memory amountReceived,
+      string memory eventType,
+      address[] memory tokensSent,
+      address[] memory tokensReceived,
+      uint256[] memory amountSent,
+      uint256[] memory amountReceived
     )
   {
+    if(uint(_additionalArgs[0]) == uint(DefiActions.CompoundLoan)){
+     (tokensSent,
+      tokensReceived,
+      amountSent,
+      amountReceived) = compoundMint(_data);
 
+      eventType = "COMPOUND_LOAN";
+    }
+    else if(uint(_additionalArgs[1]) == uint(DefiActions.CompoundReedem)){
+      (tokensSent,
+       tokensReceived,
+       amountSent,
+       amountReceived) = compoundRedeemByPercent(_data);
+
+      eventType = "COMPOUND_REDEEM";
+    }
+    else{
+      revert("Unknown action");
+    }
   }
 
   // get underlying by cToken
@@ -60,19 +98,27 @@ contract DefiPortal {
     return CToken(_cToken).underlying();
   }
 
-  // emit Loan(_cToken, receivedAmount, underlying, _amount);
+
   /**
   * @dev buy Compound cTokens
-  *
-  * @param _amount       amount of ERC20 or ETH
-  * @param _cToken       cToken address
   */
-  function compoundMint(uint256 _amount, address _cToken)
-   external
-   returns(uint256)
+  function compoundMint(bytes memory _data)
+   private
+   returns(
+     address[] memory tokensSent,
+     address[] memory tokensReceived,
+     uint256[] memory amountSent,
+     uint256[] memory amountReceived
+   )
   {
-    uint256 receivedAmount = 0;
+    uint256 receivedAmount;
+    address underlyingAddress;
+
+    (uint256 _amount,
+     address _cToken) = abi.decode(_data, (uint256, address));
+
     if(_cToken == address(cEther)){
+      underlyingAddress = ETH_TOKEN_ADDRESS;
       // mint cETH
       cEther.mint.value(_amount)();
       // transfer received cETH back to fund
@@ -81,35 +127,47 @@ contract DefiPortal {
     }else{
       // mint cERC20
       CToken cToken = CToken(_cToken);
-      address underlyingAddress = cToken.underlying();
+      underlyingAddress = cToken.underlying();
       _transferFromSenderAndApproveTo(IERC20(underlyingAddress), _amount, address(_cToken));
       cToken.mint(_amount);
       // transfer received cERC back to fund
       receivedAmount = cToken.balanceOf(address(this));
       cToken.transfer(msg.sender, receivedAmount);
     }
-
-    require(receivedAmount > 0, "received amount can not be zerro");
-
+    // Additional check
+    require(receivedAmount > 0, "Comp cToken cant be 0");
+    // Update token type
     tokensTypes.addNewTokenType(_cToken, "COMPOUND");
-    return receivedAmount;
+
+    // return DATA
+    tokensSent = new address[](1);
+    tokensSent[0] = underlyingAddress;
+    tokensReceived = new address[](1);
+    tokensReceived[0] = _cToken;
+    amountSent = new uint256[](1);
+    amountSent[0] = _amount;
+    amountReceived = new uint256[](1);
+    amountReceived[0] = receivedAmount;
   }
 
 
-  // emit Redeem(_cToken, amount, underlying, receivedAmount);
   /**
   * @dev sell certain percent of Ctokens to Compound
-  *
-  * @param _percent      percent from 1 to 100
-  * @param _cToken       cToken address
   */
-  function compoundRedeemByPercent(uint _percent, address _cToken)
-   external
-   override
-   returns(uint256)
+  function compoundRedeemByPercent(bytes memory _data)
+   private
+   returns(
+     address[] memory tokensSent,
+     address[] memory tokensReceived,
+     uint256[] memory amountSent,
+     uint256[] memory amountReceived
+   )
   {
-    uint256 receivedAmount = 0;
+    (uint256 _percent,
+     address _cToken) = abi.decode(_data, (uint256, address));
 
+    uint256 receivedAmount;
+    address underlyingAddress;
     uint256 amount = getPercentFromCTokenBalance(_percent, _cToken, msg.sender);
 
     // transfer amount from sender
@@ -117,6 +175,7 @@ contract DefiPortal {
 
     // reedem
     if(_cToken == address(cEther)){
+      underlyingAddress = ETH_TOKEN_ADDRESS;
       // redeem compound ETH
       cEther.redeem(amount);
       // transfer received ETH back to fund
@@ -128,15 +187,23 @@ contract DefiPortal {
       CToken cToken = CToken(_cToken);
       cToken.redeem(amount);
       // transfer received ERC20 back to fund
-      address underlyingAddress = cToken.underlying();
+      underlyingAddress = cToken.underlying();
       IERC20 underlying = IERC20(underlyingAddress);
       receivedAmount = underlying.balanceOf(address(this));
       underlying.transfer(msg.sender, receivedAmount);
     }
+    // Additional check
+    require(receivedAmount > 0, "Comp underlying cant be 0");
 
-    require(receivedAmount > 0, "received amount can not be zerro");
-
-    return receivedAmount;
+    // return DATA
+    tokensSent = new address[](1);
+    tokensSent[0] = _cToken;
+    tokensReceived = new address[](1);
+    tokensReceived[0] = underlyingAddress;
+    amountSent = new uint256[](1);
+    amountSent[0] = amount;
+    amountReceived = new uint256[](1);
+    amountReceived[0] = receivedAmount;
   }
 
   /**
@@ -148,7 +215,6 @@ contract DefiPortal {
   */
   function getPercentFromCTokenBalance(uint _percent, address _cToken, address _holder)
    public
-   override
    view
    returns(uint256)
   {
@@ -163,5 +229,20 @@ contract DefiPortal {
       // not correct percent
       return 0;
     }
+  }
+
+  /**
+  * @dev Transfers tokens to this contract and approves them to another address
+  *
+  * @param _source          Token to transfer and approve
+  * @param _sourceAmount    The amount to transfer and approve (in _source token)
+  * @param _to              Address to approve to
+  */
+  function _transferFromSenderAndApproveTo(IERC20 _source, uint256 _sourceAmount, address _to) private {
+    require(_source.transferFrom(msg.sender, address(this), _sourceAmount));
+    // reset previos approve because some tokens require allowance 0
+    _source.approve(_to, 0);
+    // approve
+    _source.approve(_to, _sourceAmount);
   }
 }
